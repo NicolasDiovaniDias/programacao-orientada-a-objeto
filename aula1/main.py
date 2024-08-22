98fdd5f2978c198e9a0f4c1d9935cc11a39c1e39
NicolasDiovaniDias <nicolasdiovanioliveira@gmail.com>
2024-08-22 10:23:21 -0300

    Update main.py

--- aula1/main.py
+++ aula1/main.py
@@ -4,6 +4,13 @@ class Conta:
     def listar(self):
         print(self.numero)
         print(self.saldo)
+    def sacar(self,saque):
+        self.saldo-=saque
+        self.listar()
+    def depositar(self,depositar):
+        self.saldo+=depositar
+        self.listar()
+
 if __name__ == '__main__':
     conta_nicolas = Conta()
     conta_nicolas.numero="100"
@@ -13,3 +20,5 @@ if __name__ == '__main__':
     conta_pedro.numero = "200"
     conta_pedro.saldo = 200
     conta_pedro.listar()
+    deposito=int(input("quanto voce quer depositar? "))
+    conta_nicolas.depositar(deposito)
