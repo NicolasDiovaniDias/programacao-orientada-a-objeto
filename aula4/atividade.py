7230d0da639048ce8f415e59fd3b006407208060
NicolasDiovaniDias <nicolasdiovanioliveira@gmail.com>
2024-09-12 11:17:04 -0300

    Update atividade.py

--- aula4/atividade.py
+++ aula4/atividade.py
@@ -1,9 +1,9 @@
 class carro:
-    def __init__(self, marca, modelo, ano, proprietario):
+    def __init__(self, marca, modelo, ano):
         self.marca=marca
         self.modelo=modelo
         self.ano=ano
-        self.proprietario=proprietario
+        self.proprietario=None
     def listar(self):
         print(f'''
             marca = {self.marca}
@@ -12,6 +12,8 @@ class carro:
             nome_proprietario = {self.proprietario.nome}
             idade_proprietario = {self.proprietario.idade}
         ''')
+    def definir_proprietario(self,proprietario):
+        self.proprietario=proprietario
 class proprietario:
     def __init__(self,nome,idade):
         self.nome=nome
@@ -22,5 +24,6 @@ def retorno():
     return (nome,idade)
 nome,idade=retorno()
 proprietario1=proprietario(nome,idade)
-carro1=carro("bmw","sandero","1023",proprietario1)
+carro1=carro("bmw","sandero","1023")
+carro1.definir_proprietario(proprietario1)
 carro1.listar()
